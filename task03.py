a3a9e2f795da58411dd865bcc959ea200ffdef03
«Buldakova <79263143446@mail.ru>
2022-01-24 17:33:31 +0300

    task03

---
+++ task03.py
@@ -0,0 +1,6 @@
+#  3. По заданному номеру дня недели вывести его название
+
+list=[None, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+print('Ввудите число от 1 до 7')
+a=int(input())
+print(list[a])   
