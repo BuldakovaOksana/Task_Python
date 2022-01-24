4021a53ced95022d8fc2b994e821a7dc4eb1ba2f
«Buldakova <79263143446@mail.ru>
2022-01-24 16:55:08 +0300

    task02

---
+++ task02.py
@@ -0,0 +1,8 @@
+# 2. Даны два числа. Показать большее и меньшее число
+def Show_min_max(a,b):
+    if a>b:
+        print('max = ',a,'min = ',b)
+    else:
+        print('max = ',b,'min = ',a)
+
+Show_min_max(7,9)     
