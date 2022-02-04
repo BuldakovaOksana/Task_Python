3ae49e83e348d14273e585615aa022348e07843c
«Buldakova <79263143446@mail.ru>
2022-02-04 20:57:21 +0300

    new_task24

--- New_task/new_task24.py
+++ New_task/new_task24.py
@@ -1,6 +1,17 @@
 # 24. В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением дробной части элементов. 
 # Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-ls=[1.1, 1.2, 3.1, 5, 10.01]
 
-for i in 
+
+ls=[1.1, 1.2, 3.1, 5, 10.01]
+li=[i for i in ls if type(i)==float]
+li=list(map(lambda i: round(i%1,2), li))
+print(li)
+
+def res(list):
+    for i in list:
+        if type(i)==float:
+           return max(list)-min(list)
+            
+
+print(res(li))
