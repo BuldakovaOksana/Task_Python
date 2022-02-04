112c3571217ad9980d91a505e9779649d8684c88
«Buldakova <79263143446@mail.ru>
2022-02-04 22:05:12 +0300

    new_task25

--- New_task/new_task25.py
+++ New_task/new_task25.py
@@ -1,2 +1,14 @@
 # 25. Написать программу преобразования десятичного числа в двоичное
 
+from audioop import reverse
+
+
+def conver(a):
+    list=[]
+    while a>0:
+        list.append(a%2)
+        a//=2
+    list.reverse()
+    return list
+print(conver(20))      
+
